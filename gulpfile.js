300c82932a4dc2c5fc4ca90d6d425b09c46a5e74
j-heavy <mr.nikitachurakov@gmail.com>
2018-07-08 23:38:39 +0500

    sw fixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,18 @@
 var gulp = require('gulp');
+var csso = require('gulp-csso');
 
-gulp.task('mytask', function () {
-	return gulp.src('source-files');
-	.pipe(plugin());
+gulp.task('default', function () {
+    return gulp.src('./styles.css')
+        .pipe(csso());
+        .pipe(gulp.dest('./dest'));
+});
+
+gulp.task('development', function () {
+    return gulp.src('/_site/styles.css')
+        .pipe(csso({
+            restructure: false,
+            sourceMap: true,
+            debug: true
+        }))
+        .pipe(gulp.dest('/dest'));
 });
---
+++ main.js
@@ -0,0 +1,7 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+      .then(() => navigator.serviceWorker.ready.then((worker) => {
+        worker.sync.register('syncdata');
+      }))
+      .catch((err) => console.log(err));
+}
