e71a906c16c1f99abb12ebae4ac51b046cb5a765
j-heavy <mr.nikitachurakov@gmail.com>
2018-07-09 21:14:28 +0500

    compressed

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ var gulp = require('gulp');
 var csso = require('gulp-csso');
 
 gulp.task('default', function () {
-    return gulp.src('./styles.css')
+    return gulp.src('/styles.css')
         .pipe(csso());
-        .pipe(gulp.dest('./dest'));
+        .pipe(gulp.dest('/dest'));
 });
 
-gulp.task('development', function () {
+gulp.task('minifycss', function () {
     return gulp.src('/_site/styles.css')
         .pipe(csso({
             restructure: false,
